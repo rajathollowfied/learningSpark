e3cc7eb44ed1ea06fabf189550ce42c674169b3d
Rajat Sharma <rajat.hollowfied@gmail.com>
2023-07-25 01:33:51 +0530

    title lookup added

--- famousMovie.py
+++ famousMovie.py
@@ -1,8 +1,18 @@
+import findspark,codecs
+findspark.init()
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as sqlFunc
 from pyspark.sql.types import StructField,StructType,IntegerType,LongType
 
 spark = SparkSession.builder.appName("famousMovie").getOrCreate()
+
+def loadMovieNames():
+    movieNames = {}
+    with codecs.open("D:\Learning\GIT\datasets\ml-100k//u.ITEM","r",encoding='ISO-8859-1',errors='ignore') as titleFile:
+        for line in titleFile:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
 
 schema = StructType ([  \
     StructField("userID",IntegerType(), True),    \
@@ -11,8 +21,24 @@ schema = StructType ([  \
     StructField("timeStamp",LongType(),True)  \
 ])
 
-moviesDF = spark.read.options(delimiter = '\t').schema(schema).csv("D:\Learning\GIT\datasets\ml-100k//u.data").crea
+titleDict = spark.sparkContext.broadcast(loadMovieNames())
 
-famousMovie = spark.sql("")
+moviesDF = spark.read.options(delimiter = '\t').schema(schema).csv("D:\Learning\GIT\datasets\ml-100k//u.data")
+moviesDF.createOrReplaceTempView("movies")
+famousMovies = spark.sql(\
+    "select movieID,count(1) as cnt from movies\
+        group by movieID order by count(1) desc"\
+            )
+
+def lookUpTitle(movieID):
+    return titleDict.value[movieID]
+
+lookUpTitleUDF = sqlFunc.udf(lookUpTitle)
+
+famousMoviesWithTitles = famousMovies.withColumn(\
+    "movieTitle",lookUpTitleUDF(sqlFunc.col("movieID"))\
+        )
+
+famousMoviesWithTitles.show(10, False)
 
 spark.stop()
